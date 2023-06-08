69405c1c242260e5177017bd3df8d943d5d4d137
AchalRaghuveer <raghuveerachal@gmail.com>
2023-06-08 17:20:35 -0500

    q0 completed

--- app.py
+++ app.py
@@ -31,30 +31,69 @@ def searchName():
     descript = data[4]
     # image_paths = [row[0] for row in data if row[0].strip() != '']
     # print("output", image_paths)
-    link="https://abr2435assign1.blob.core.windows.net/quiz0contain/"+img
+    if img != None:
+        link="https://abr2435assign1.blob.core.windows.net/quiz0contain/"+img
+    else:
+        link="No image Found"
     print('link----->', link)
-    print('link2--->https://abr2435assign1.blob.core.windows.net/abr2435container/chuck.jpg')
     return render_template('index.html', imgLink=link, room = room, name=name, descript=descript)
 
-@app.route("/money", methods=['GET', 'POST'])
+@app.route("/range", methods=['GET', 'POST'])
 def moneyRange():
     number1 = request.form.get('number1')
     number2 = request.form.get('number2')
     cursor = connection.cursor()
-    cursor.execute("select picture from dbo.[people] where salary between ? and ? ",(number1, number2))
+    cursor.execute("select * from dbo.[q0c] where room between ? and ? ",(number1, number2))
     data = cursor.fetchall()
     linkVals = []
+    # nameVals = []
+    # discriptVals = []
     if data:
         print("result = ", data)
-        image_paths = [row[0] for row in data if row[0].strip() != '']
-        print('after sorting', image_paths)
-        for i in range(len(image_paths)):
-            print('img val', image_paths[i])
-            if len(image_paths[i]) > 0:
-                linkVals.append("https://abr2435assign1.blob.core.windows.net/abr2435container/" + str(image_paths[i]))
-                print('vals =====> https://abr2435assign1.blob.core.windows.net/abr2435container/', str(image_paths[i]))
+        for i in range(len(data)):
+            print('img val', data[i])
+            # nameVals.append(image_paths[i][0])
+            # discriptVals.append(image_paths[i][4])
+            if str(data[i][2]) != None:
+                img = "https://abr2435assign1.blob.core.windows.net/quiz0contain/" + str(data[i][2])
+            else:
+                img = "No Image Found"
+            print('image value -------> ', img)
+            val = ValuesOBJ(data[i][0],img , data[i][4])
+            linkVals.append(val)
+            print('vals =====>', val)
 
     return render_template('index.html', linkVals=linkVals)
+
+@app.route("/changeName", methods=['GET', 'POST'])
+def changeDescript():
+    name = request.form.get('name')
+    discript = request.form.get('discription')
+    # print('search', search_query)
+    cursor = connection.cursor()
+    # cursor.execute("select * from dbo.[people]")
+    # cursor.execute("select * from dbo.[q0c] where teln = ?", search_query)
+    cursor.execute("Update dbo.[q0c] set descript = ? where name = ?", (discript, name))
+    connection.commit()
+    cursor.execute("select * from dbo.[q0c] where name = ?", name)
+    data = cursor.fetchone()
+    print("result = ", data)
+    name = data[0]
+    descript = data[4]
+    # image_paths = [row[0] for row in data if row[0].strip() != '']
+    # print("output", image_paths)
+    return render_template('index.html', name3=name, descript3=descript)
+
+class ValuesOBJ:
+            
+         
+    # The init method or constructor
+    def __init__(self, name, link, descript):
+             
+        # Instance Variable
+        self.name = name
+        self.link = link
+        self.descript = descript            
 
 if __name__ == "__main__":
     app.run(debug = True)
