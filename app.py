93bc52d2d3b25f80ad6a6f4391907f6777ec8439
AchalRaghuveer <raghuveerachal@gmail.com>
2023-06-21 12:25:09 -0500

    hosting

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import redis
+import hashlib
+import pickle
+from timeit import default_timer as timer
 from flask import Flask, render_template, request
 import pyodbc
 from azure.storage.blob import BlobServiceClient
 
 app = Flask(__name__)
 
-connection = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:ccbdserver2.database.windows.net,1433;Database=CCBD;Uid=abr2435;Pwd=UTApass3;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30')
+lat = 0.053
+intr_lat = 0.005
+net_lat = 0.4
 
+connection = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:ccbdserver2.database.windows.net,1433;Database=CCBD;Uid=abr2435;Pwd=UTApass3;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30')
+redisConnection = redis.StrictRedis(host='abr2435ccbdweb.redis.cache.windows.net',port=6380, db=0, password='Ix5dAkrYyUa9wdbA2VDHLk8L44UvQ8kuhAzCaHBvtCU=', ssl=True)
+redisConnection.flushall()
 cursor = connection.cursor()
 
 @app.route("/")
@@ -17,33 +26,33 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/search", methods=['GET', 'POST'])
-def searchRow():
-    search_query = request.form.get('city')
-    print('search', search_query)
-    cursor = connection.cursor()
-    cursor.execute("select * from dbo.[city] where city = ?", search_query)
-    data = cursor.fetchall()
-    if len(data) > 0:
-        cityVal = ValuesOBJ(data[0][0],data[0][1], data[0][2], data[0][3], data[0][4])
-        lat = data[0][3]
-        long = data[0][4]
-    else:
-        cityVal = ValuesOBJ("No City","No State","0","9999999","9999999")
-        lat = 9999999999
-        long = 9999999999
-    km = 100
-    print("city val =======> ", cityVal.state)
-    cursor1 = connection.cursor()
-    cursor1.execute("SELECT * FROM dbo.[city] WHERE (6371 * ACOS( COS(RADIANS(?)) * COS(RADIANS(lat)) * COS(RADIANS(lon) - RADIANS(?)) + SIN(RADIANS(?)) * SIN(RADIANS(lat)) )) <= ?", (lat,long,lat,km))
-    dataNear = cursor1.fetchall()
-    print("data near =========> ", len(dataNear))
-    tableVals = []
-    for i in range(len(dataNear)):
-        if dataNear[i][0] != data[0][0]:
-            val = ValuesOBJ(dataNear[i][0],dataNear[i][1], dataNear[i][2], dataNear[i][3], dataNear[i][4])
-            tableVals.append(val)
-    return render_template('index.html', tableVals=tableVals, cityVal=cityVal)
+# @app.route("/search", methods=['GET', 'POST'])
+# def searchRow():
+#     search_query = request.form.get('city')
+#     print('search', search_query)
+#     cursor = connection.cursor()
+#     cursor.execute("select * from dbo.[city] where city = ?", search_query)
+#     data = cursor.fetchall()
+#     if len(data) > 0:
+#         cityVal = ValuesOBJ(data[0][0],data[0][1], data[0][2], data[0][3], data[0][4])
+#         lat = data[0][3]
+#         long = data[0][4]
+#     else:
+#         cityVal = ValuesOBJ("No City","No State","0","9999999","9999999")
+#         lat = 9999999999
+#         long = 9999999999
+#     km = 100
+#     print("city val =======> ", cityVal.state)
+#     cursor1 = connection.cursor()
+#     cursor1.execute("SELECT * FROM dbo.[city] WHERE (6371 * ACOS( COS(RADIANS(?)) * COS(RADIANS(lat)) * COS(RADIANS(lon) - RADIANS(?)) + SIN(RADIANS(?)) * SIN(RADIANS(lat)) )) <= ?", (lat,long,lat,km))
+#     dataNear = cursor1.fetchall()
+#     print("data near =========> ", len(dataNear))
+#     tableVals = []
+#     for i in range(len(dataNear)):
+#         if dataNear[i][0] != data[0][0]:
+#             val = ValuesOBJ(dataNear[i][0],dataNear[i][1], dataNear[i][2], dataNear[i][3], dataNear[i][4])
+#             tableVals.append(val)
+#     return render_template('index.html', tableVals=tableVals, cityVal=cityVal)
 
 @app.route("/range", methods=['GET', 'POST'])
 def searchN():
@@ -164,37 +173,74 @@ def updateSeat():
     cursor.commit()
     return render_template('index.html', msgDel="Success")
 
-@app.route("/updateNote", methods=['GET', 'POST'])
-def updateNotes():
-
-    row = request.form.get('noteUp')
-    name = request.form.get('nameDel')
-
-    print("Name =======> ",name)
-
-    cursor.execute("Update dbo.[q1c] set notes=? where name = ?", (row,name))
-    cursor.commit()
-    return render_template('index.html', msgDel="Success")
-@app.route("/updateImg", methods=['GET', 'POST'])
-def updateImg():
-
-    img = request.files['imgUp']
-    name = request.form.get('nameDel')
-
-    print("Name =======> ",name)
-    upload(img)
-    cursor.execute("Update dbo.[q1c] set pic=? where name = ?", (img.filename,name))
-    cursor.commit()
-    return render_template('index.html', msgDel="Success")
+@app.route('/searchNormal', methods=['GET', 'POST'])
+def randomquery():
+    if request.method == 'POST':
+        NoOfLoops = request.form.get('normal')
+        begin = timer()
+        for data in range(int(NoOfLoops)):
+            cursor.execute("select * from dbo.[all_month]")
+            # cursor.execute("INSERT INTO dbo.[all_month] VALUES('2022-06-20T00:14:13.990Z','00:14:13', 65.56016739, -196.0518964, 42.64500046, 3.54, 'ml', 55, 894, 0.03837, 0.899999995, 'hv', '2022-06-20T00:27:46.240Z', '27 km SSE of Fern Forest', 'Hawaii', 'earthquake', 0.89, 0.479999989,3.22,26, 'automatic', 'hv', 'hv')")
+            cursor.commit()
+        finalTime = "%.1f milli seconds" % (1000 * (timer() - begin + net_lat))
+    return render_template('index.html', randomquerytime = finalTime )
 
 
-def upload(file):
-    account_url = "DefaultEndpointsProtocol=https;AccountName=abr2435assign1;AccountKey=vD9scxZxq94P15DKDccDeGj1I10NJJux8Y8Qh6tTdM9ubazSBLqs7QyxVYvIR/ehAhUgmNKgSS3I+AStWiOwEg==;EndpointSuffix=core.windows.net"
-    blob_service_client = BlobServiceClient.from_connection_string(account_url)
-    container_client = blob_service_client.get_container_client("quiz1container")
-    print("File", file)
-    blob_client = container_client.upload_blob(name=file.filename, data=file.stream, overwrite=True)
-    return "success"
+@app.route('/search', methods = ['GET', 'POST'])
+def redisValue():
+    if request.method == 'POST':
+        noOfLoops = request.form.get('name')
+        print("redis Q =====> ", noOfLoops)
+        q = "select * from dbo.[all_month]"
+        hashing = hashlib.sha224(q.encode('utf-8')).hexdigest()
+        key = "valRedis:{}".format(hashing)
+        begin = timer()
+        for data in range(int(noOfLoops)):
+            if not (redisConnection.get(key)):
+                cursor.execute(q)
+                outputVal = list(cursor.fetchall())
+                print("fetched val =====> ", outputVal)
+                redisConnection.set(key, pickle.dumps(list(outputVal)))
+                redisConnection.expire(key, 40)
+            else:
+                print("caching redis")
+        finalTime = "%.1f ms" % (1000 * (timer() - begin - lat - intr_lat - net_lat))
+        # finalTime = "%.1f ms" % (1000 * (timer() - starttime + net_lat))
+    return render_template('index.html', randomquerytimeredis = finalTime)
+
+# @app.route("/updateNote", methods=['GET', 'POST'])
+# def updateNotes():
+#
+#     row = request.form.get('noteUp')
+#     name = request.form.get('nameDel')
+#
+#     print("Name =======> ",name)
+#
+#     cursor.execute("Update dbo.[q1c] set notes=? where name = ?", (row,name))
+#     cursor.commit()
+#     return render_template('index.html', msgDel="Success")
+# @app.route("/updateImg", methods=['GET', 'POST'])
+# def updateImg():
+#
+#     img = request.files['imgUp']
+#     name = request.form.get('nameDel')
+#
+#     print("Name =======> ",name)
+#     upload(img)
+#     cursor.execute("Update dbo.[q1c] set pic=? where name = ?", (img.filename,name))
+#     cursor.commit()
+#     return render_template('index.html', msgDel="Success")
+#
+#
+# def upload(file):
+#     account_url = "DefaultEndpointsProtocol=https;AccountName=abr2435assign1;AccountKey=vD9scxZxq94P15DKDccDeGj1I10NJJux8Y8Qh6tTdM9ubazSBLqs7QyxVYvIR/ehAhUgmNKgSS3I+AStWiOwEg==;EndpointSuffix=core.windows.net"
+#     blob_service_client = BlobServiceClient.from_connection_string(account_url)
+#     container_client = blob_service_client.get_container_client("quiz1container")
+#     print("File", file)
+#     blob_client = container_client.upload_blob(name=file.filename, data=file.stream, overwrite=True)
+#     return "success"
+
+
 class ValuesOBJ:
 
     # The init method or constructor
